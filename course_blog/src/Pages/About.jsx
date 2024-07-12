54eb757fa95688ad52780eae7fa25c763bca7c40
deepak <raydeepak4596@gmail.com>
2024-07-13 00:40:13 +0530

    third commit

--- course_blog/src/Pages/About.jsx
+++ course_blog/src/Pages/About.jsx
@@ -7,8 +7,8 @@ const About = () => {
             <Box>
                 <Typography textAlign='center'><h1>About</h1></Typography>
                 <Divider />
-                <Container sx={{ display: 'flex',textAlign:'justify', justifyContent: 'center',marginTop:'2rem', alignItems: 'center' }}>
-                    <Box flex={2}>
+                <Container  sx={{ display: 'flex',textAlign:'justify', flexDirection:{xs:"column",sm:'row'}, justifyContent: 'center',marginTop:'2rem', alignItems: 'center' }}>
+                    <Box flex={1}  sx={{order:{xs:2,sm:1 },marginTop:{xs:'2rem',sm:'0rem'}}}>
                     <Typography sx={{ marginRight: '2rem' }}>
                         Hello! I'm Deepak, a dedicated and passionate web developer with a Bachelor's degree in Computer Applications (BCA). I specialize in the MERN stack, which includes MongoDB, Express, React, and Node.js. With a solid foundation in both front-end and back-end technologies, I excel in building dynamic, efficient, and scalable web applications.
 
@@ -36,7 +36,7 @@ const About = () => {
                       <b>  E-commerce Website:</b> Created an e-commerce website with a comprehensive shopping cart, user authentication, product management, and order processing. Leveraged the MERN stack to ensure seamless user experiences and efficient server-client communication.
                     </Typography>
                     </Box>
-                    <Box flex={2}>
+                    <Box flex={1} sx={{order:{xs:1,sm:2}}} >
                         <Card component='img' sx={{ width: '100%', height: 'auto' }} src='https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg
 '/>
                     </Box>
