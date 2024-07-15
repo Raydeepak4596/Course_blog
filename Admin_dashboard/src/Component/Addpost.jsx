0fd5a792004e0efec8dd351692f7188370dc508b
deepak <raydeepak4596@gmail.com>
2024-07-16 01:55:54 +0530

    editor addes

--- Admin_dashboard/src/Component/Addpost.jsx
+++ Admin_dashboard/src/Component/Addpost.jsx
@@ -1,43 +1,49 @@
 import { Box, Button, InputBase, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import axios from 'axios'
-
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import DOMPurify from 'dompurify';
 
 const Addpost = () => {
-  const[data,setdata]=useState({
-    title:'',
-    description:'',
-    image:''
+  const [data, setdata] = useState({
+    title: '',
+    description: '',
+    image: ''
   })
-const handledata=(e)=>{
-  const{name,value}=e.target
-  setdata({...data,[name]:value})
-}
-const imghandle=(e)=>{
-  console.log(e.target.files[0])
-  setdata({...data,image:e.target.files[0]})
-
-}
-
-const btnadd=(e)=>{
-  e.preventDefault()
-  try {
-    const formdata=new FormData()
-    formdata.append('title',data.title)
-    formdata.append('description',data.description)
-    formdata.append('image',data.image)
- 
-    axios.post(`http://localhost:4000/api/vi/addpost`,formdata)
-    setdata({title:'',description:'',image:''})
-    e.target.reset()
-    
-  } catch (error) {
-    
+  const handledata = (e) => {
+    const { name, value } = e.target
+    setdata({ ...data, [name]: value })
   }
-}
+  const imghandle = (e) => {
+    console.log(e.target.files[0])
+    setdata({ ...data, image: e.target.files[0] })
 
+  }
+
+  const btnadd = (e) => {
+    e.preventDefault()
+    try {
+      const formdata = new FormData()
+      formdata.append('title', data.title)
+      formdata.append('description', data.description)
+      formdata.append('image', data.image)
+
+      axios.post(`http://localhost:4000/api/vi/addpost`, formdata)
+      setdata({ title: '', description: '', image: '' })
+      e.target.reset()
+
+    } catch (error) {
+
+    }
+  }
+
+  const handledatadescription = (e) => {
+    const sanitizedDescription = DOMPurify.sanitize(e);
+    setdata({ ...data, description: sanitizedDescription });
+  }
   return (
-    <Box 
+    <Box
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -57,24 +63,24 @@ const btnadd=(e)=>{
       </Typography>
       <form onSubmit={btnadd} style={{ width: '100%' }}>
         <InputBase onChange={imghandle}
-          type="file" accept='image/*' 
+          type="file" accept='image/*'
           sx={{ mb: 2, width: '100%', p: 1, border: '1px solid #ccc', borderRadius: '4px' }}
         />
         <InputBase
-          placeholder="Title" name='title' value={data.title}  onChange={handledata}
+          placeholder="Title" name='title' value={data.title} onChange={handledata}
           sx={{ mb: 2, width: '100%', p: 1, border: '1px solid #ccc', borderRadius: '4px' }}
         />
-        <TextField name='description' value={data.description} onChange={handledata}
+        <ReactQuill value={data.description} onChange={handledatadescription}
           placeholder="Description"
-          multiline
-          rows={10}
-          variant="outlined"
+
+
           sx={{ mb: 2, width: '100%' }}
         />
         <Button variant="contained" color="primary" type="submit" fullWidth>
           Add
         </Button>
       </form>
+
     </Box>
   );
 };
